9f9c07c394bc6f4a0cf3fc88365658c3828921be
AgustinFassola <agusfassolacarp@gmail.com>
2022-06-06 11:49:07 -0300

    colores en el navBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-import LogoImg from './assets/simples_acciones.png';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <header className="App-header">
         <NavBar/>
         <h1 style={stylesH1}>Bienvenidos a simples acciones</h1>
-        <img alt='logo coder' src={LogoImg}></img>
         <p style={{marginTop: "120px"}}>compra tus bolsas</p>
         <a className="App-link"
           href="https://reactjs.org"
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,19 @@
 import "./navBar.css";
+import LogoImg from '../assets/simples_acciones.png';
 
 export default function NavBar(){
     return(
         <nav className="nav-main">
-            <ul>
-                <li>Categoria A</li>
-                <li>Categoría B</li>
+            <img className="nav-img" alt='logo coder' src={LogoImg}></img>
+            <ul className="barra ">
+                <li>Inicio</li>
+                <li>Productos
+                    <ul>
+                        <li>Bolsas compostables</li>
+                        <li>Descartables compostables</li>
+                    </ul> 
+                </li>
+                <li>Contacto</li>
             </ul>
         </nav>
     );
