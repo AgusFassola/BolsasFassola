6d5f428149b5d8e2fc9caf21c055fbb87b06e3b9
AgustinFassola <agusfassolacarp@gmail.com>
2022-06-06 00:55:14 -0300

    NavBar estructura basica

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
+import LogoImg from './assets/simples_acciones.png';
+import NavBar from './components/NavBar';
+
 function App() {
+
+  const stylesH1={color:'blue',}
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+        <NavBar/>
+        <h1 style={stylesH1}>Bienvenidos a simples acciones</h1>
+        <img alt='logo coder' src={LogoImg}></img>
+        <p style={{marginTop: "120px"}}>compra tus bolsas</p>
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import "./navBar.css";
+
+export default function NavBar(){
+    return(
+        <nav className="nav-main">
+            <ul>
+                <li>Categoria A</li>
+                <li>Categoría B</li>
+            </ul>
+        </nav>
+    );
+}
