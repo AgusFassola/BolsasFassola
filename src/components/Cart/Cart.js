b6cc6aa3f1f1f6bd88f0ab8e43a53c7850d9105e
AgustinFassola <agusfassolacarp@gmail.com>
2022-07-04 23:57:40 -0300

    implementacion de cartView

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,13 +3,26 @@ import { useContext } from 'react';
 import CartContext from '../context/CartContext';
 
 export default function Cart() {
-  const contexto=useContext(CartContext);
-  console.log(contexto);
+  const {cart}=useContext(CartContext);
+  console.log(cart);
+  if(cart.length===0){
+    return <h1>nada en el carrito</h1>
+  }
   return (
-    <div>Cart
       <div>
+        {
+            cart.map(item =>(
+              <div className='mb-5 py-3 text-center' key={item.id}>
+                <h3>
+                  {item.name}
+                </h3>
+                <p>{item.desripcion}</p>
+                <p>${item.price}</p>
+              </div>
+            ))
+        }
+        
         <button>limpiar carrito</button>
       </div>
-    </div>
   )
 }
