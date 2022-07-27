3c77d082869a8886edc017ffac99045212b19e35
AgustinFassola <agusfassolacarp@gmail.com>
2022-07-27 00:00:01 -0300

    arreglos generales

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { useContext } from 'react';
 import CartContext from '../context/CartContext';
-import { createBuyOrder } from '../firebase/firestore';
-import CartForm from './CartForm';
-//                <p>total a pagar: ${totalPriceCart().toFixed(2)}{""}</p>
+
+
 
 export default function Cart() {
 
   const {cart,removeItemFromCart,clearCart}=useContext(CartContext);
 
-  
-
-
-
-  
   if(cart.length===0){
     return <h1>nada en el carrito</h1>
   }
@@ -34,8 +28,7 @@ export default function Cart() {
               </div>
             ))
         }
-        <CartForm cart={cart} clearCart={clearCart} createBuyOrder={createBuyOrder}/>
-        <button onClick={handleBuyOrder}>Finalizar compra</button>
+
         <button onClick={clearCart}>limpiar carrito</button>
         
       </div>
